d0d49d8966b8531ef1e7795ccbf1c9a060934f45
Jigang Kim <jgkim2020@snu.ac.kr>
2022-01-17 21:04:34 +0900

    Remove deprecated code

--- example.py
+++ example.py
@@ -6,53 +6,6 @@ from nvidia_gpu_scheduler.worker import NVGPUWorker
 from nvidia_gpu_scheduler.utils import CatchExceptions
 
 
-# def child_process(args):
-#     import os
-#     import random
-#     import time
-#     from tqdm import tqdm
-#     from nvidia_gpu_scheduler.utils import log_tqdm
-
-#     n = 10
-#     pbar = tqdm(total=n)
-#     config_filename = os.path.basename(args.config_dir)
-#     log_tqdm(pbar, args.config_dir.replace('/','_'))
-#     for i in range(n):
-#         time.sleep(1)
-#         pbar.update(n=1)
-#         log_tqdm(pbar, args.config_dir.replace('/','_'))
-#     log_tqdm(pbar, args.config_dir.replace('/','_'), remove=True)
-#     pbar.close()
-#     # intentionally trigger with 50% probability
-#     trigger_error = random.choice([True, False])
-#     if trigger_error:
-#         raise ValueError('failed with 50% probability')
-#     else:
-#         print('succeeded with 50% probability')
-
-
-# def child_process_args(f, logging=False, **kwargs):
-#     import json
-#     from types import SimpleNamespace
-
-#     # reconstruct arguments
-#     params = json.load(f, object_hook=lambda d : SimpleNamespace(**d)) # read json as namespace
-#     args = SimpleNamespace()
-#     if hasattr(params, 'ddpg'):
-#         args.config_type = 'ddpg'
-#     elif hasattr(params, 'lnt'):
-#         args.config_type = 'lnt-ddpg'
-#     else:
-#         raise ValueError('Cannot find CONFIG_TYPE!')
-#     args.config_dir = f.name
-#     args.absolute_path = True
-#     args.logging = logging
-#     args.render = False
-#     args.resume = False
-#     args.playback = False
-#     return args
-
-
 class ExampleWorkerJSON(NVGPUWorker):
     @staticmethod
     @CatchExceptions
@@ -152,48 +105,3 @@ if __name__ == "__main__":
             apply_limits='user'
         )
         worker.run()
-
-# DEPRECATED
-# if __name__ == '__main__':
-#     from nvidia_gpu_scheduler import NVGPUScheduler_deprecated
-#     '''
-#     Dummy example
-    
-#     User should provide child process function and child process argument function.
-#     Incorporating nvidia_gpu_scheduler.utils.log_tqdm into the chlid process function is recommended to track child process progress.
-
-#     '''
-#     parser = argparse.ArgumentParser(description='Simple example for NVIDIA GPU compute task scheduling utility (quad-GPU setup)')
-#     parser.add_argument('--available_gpus', nargs='+', type=int, default=[0,1,2,3],
-#         help='List of available GPU(s)'
-#     )
-#     parser.add_argument('--config_fname_extension', type=str, default='.json',
-#         help='File extension of config file(s)'
-#     )
-#     parser.add_argument('--max_gpu_utilization', nargs='+', type=int, default=[100,100,30,30],
-#         help='Maximum utilization rate for each GPU'
-#     )
-#     parser.add_argument('--max_jobs_per_gpu', nargs='+', type=int, default=[3,3,1,1],
-#         help='Maximum number of concurrent jobs'
-#     )
-#     parser.add_argument('--utilization_margin', type=int, default=5,
-#         help='Percent margin for maximum GPU utilization rate'
-#     )
-#     parser.add_argument('--time_between_tasks', type=int, default=3,
-#         help='Time delay in seconds between tasks'
-#     )
-#     parser.add_argument('--child_verbose', action='store_false',
-#         help='Allow child process(s) to output to terminal'
-#     )
-#     parser.add_argument('--logging', action='store_true',
-#         help='Enable logging for child process(s)'
-#     )
-#     args = parser.parse_args()
-    
-#     path_to_configs = str((Path(__file__).parent / 'example_configs').resolve())
-#     # 1. child process does NOT display traceback upon uncaught exception by default
-#     manager1 = NVGPUScheduler_deprecated(child_process, path_to_configs, child_args=child_process_args, **vars(args))
-#     manager1.run()
-#     # 2. use CatchExceptions wrapper to display child process traceback upon uncaught exception
-#     manager2 = NVGPUScheduler_deprecated(CatchExceptions(child_process), path_to_configs, child_args=child_process_args, **vars(args))
-#     manager2.run()
--- example_openaibaselines.py
+++ example_openaibaselines.py
@@ -16,33 +16,6 @@ except ImportError as e:
     sys.exit()
 
 
-# def child_process(packaged_args):
-#     from baselines.common.cmd_util import common_arg_parser
-#     from nvidia_gpu_scheduler.utils import log_tqdm
-#     import os
-#     from tqdm import tqdm
-
-#     args, config_dir = packaged_args # unpack
-#     # Display addtional info (real-time child process progress not working)
-#     arg_parser = common_arg_parser()
-#     parsed_args, parsed_unknown_args = arg_parser.parse_known_args(args)
-#     pbar = tqdm(total=parsed_args.num_timesteps)
-#     config_filename = os.path.basename(config_dir)
-#     log_tqdm(pbar, config_dir.replace('/','_'))
-#     # Execute OpenAI baselines.run module
-#     run_baseline(args)
-#     # Close tqdm and remove tqdm logs
-#     log_tqdm(pbar, config_dir.replace('/','_'), remove=True)
-#     pbar.close()
-
-
-# def child_process_args(f, **kwargs):
-#     import json
-
-#     params = json.load(f)
-#     return params['sys.argv'], f.name
-
-
 class OpenAIBaselinesExampleWorker(NVGPUWorker):
     @staticmethod
     @CatchExceptions
@@ -93,48 +66,3 @@ if __name__ == '__main__':
             apply_limits='user'
         )
         worker.run()
-
-# DEPRECATED
-# if __name__ == '__main__':
-#     from nvidia_gpu_scheduler import NVGPUScheduler_deprecated
-#     '''
-#     OpenAI baselines example
-    
-#     OpenAI baselines (https://github.com/openai/baselines) and its dependencies (mujoco-py, ...) are required to run this example!
-#     Tracking child process progress was NOT implemented for this example. (requires modifying the OpenAI baselines source code or some hacky workaround)
-
-#     '''
-#     parser = argparse.ArgumentParser(description='Simple example for NVIDIA GPU compute task scheduling utility (quad-GPU setup)')
-#     parser.add_argument('--available_gpus', nargs='+', type=int, default=[0,1,2,3],
-#         help='List of available GPU(s)'
-#     )
-#     parser.add_argument('--config_fname_extension', type=str, default='.json',
-#         help='File extension of config file(s)'
-#     )
-#     parser.add_argument('--max_gpu_utilization', nargs='+', type=int, default=[100,100,30,30],
-#         help='Maximum utilization rate for each GPU'
-#     )
-#     parser.add_argument('--max_jobs_per_gpu', nargs='+', type=int, default=[3,3,1,1],
-#         help='Maximum number of concurrent jobs'
-#     )
-#     parser.add_argument('--utilization_margin', type=int, default=5,
-#         help='Percent margin for maximum GPU utilization rate'
-#     )
-#     parser.add_argument('--time_between_tasks', type=int, default=15,
-#         help='Time delay in seconds between tasks'
-#     )
-#     parser.add_argument('--child_verbose', action='store_true',
-#         help='Allow child process(s) to output to terminal'
-#     )
-#     parser.add_argument('--logging', action='store_true',
-#         help='Enable logging for child process(s)'
-#     )
-#     args = parser.parse_args()
-    
-#     path_to_configs = str((Path(__file__).parent / 'example_openaibaselines_configs').resolve())
-#     # 1. child process does NOT display traceback upon uncaught exception by default
-#     manager1 = NVGPUScheduler_deprecated(child_process, path_to_configs, child_args=child_process_args, **vars(args))
-#     manager1.run()
-#     # 2. use CatchExceptions wrapper to display child process traceback upon uncaught exception
-#     manager2 = NVGPUScheduler_deprecated(CatchExceptions(child_process), path_to_configs, child_args=child_process_args, **vars(args))
-#     manager2.run()
